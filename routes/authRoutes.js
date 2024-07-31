fc126a808a01ab4f7082d047a8110f553391fe3b
nandweza <allannandweza@gmail.com>
2024-07-31 12:03:18 -0600

    updates

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,10 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router
-    .route("/register")
-    .get(authController.getRegisterPage)
-    .post(authController.registerUser);
+// router
+//     .route("/register")
+//     .get(authController.getRegisterPage)
+//     .post(authController.registerUser);
 
 router
     .route("/login")
